bee8225636fb9eeeb0afdd5c521362b1fa2c5ec4
Raviteja D <52106702+Raviteja-Guna@users.noreply.github.com>
2021-07-07 00:13:33 +0530

    Create number.h

---
+++ 3_Implementation/number.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#ifndef CONVERSION_DOT_H
+#define CONVERSION_DOT_H
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include"unity_internals.h"
+#include"unity.h"
+int bin_to_dec(char *s);
+int hex_to_dec(char *s);
+
+#endif
