421a25b8f9b2124ce7e91e76f341ea2ff0df4af1
Raviteja D <52106702+Raviteja-Guna@users.noreply.github.com>
2021-07-13 11:41:12 +0530

    Update hex_to_dec.c

--- 3_Implementation/hex_to_dec.c
+++ 3_Implementation/hex_to_dec.c
@@ -3,13 +3,13 @@
 //This function converts Hexadecimal number to decimal number
 int hex_to_dec(char *s) {
 
-int op,i,j,k,num_int=0,rem_int=0,rem;
-float rem_frac=0.0;
-char a[strlen(s)],b[strlen(s)];
+int i,j,k,num_int=0,rem_int=0;
+char a[strlen(s)];
 for(i=0,j=0,k=0;i<strlen(s);i++) {
+    
         if(isxdigit(s[i])==0){
           return -1; }
-       rem = s[i];
+       int rem = s[i];
         if(rem>=48 && rem<=57)
              rem = rem-48;
         else if(rem>=65 && rem<=70)
@@ -23,4 +23,3 @@ for(j=0,i=num_int-1;j<num_int;j++,i--) {
     rem_int=rem_int+(a[j] * pow(16,i));   }
 return rem_int;
 }
-
